75453877a4897814ff63c61eba894e5b89309565
AlbanRoullier-gall <44199380+AlbanRoullier-gall@users.noreply.github.com>
2025-12-15 16:42:02 +0100

    Fix Backoffice: add path mapping and webpack alias for @tfe/shared-types resolution

--- backoffice/next.config.js
+++ backoffice/next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, { isServer }) => {
+    // Help webpack resolve @tfe/shared-types
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@tfe/shared-types": require("path").resolve(__dirname, "node_modules/@tfe/shared-types"),
+    };
+    return config;
+  },
   async rewrites() {
     // Vérification stricte de la variable d'environnement
     const apiUrl = process.env.NEXT_PUBLIC_API_URL;
